1365ed01c30670b8c36db25a3a1b543607067e03
Cliff Lau <cliff@clifflau.hk>
2025-06-02 03:41:19 +0800

    config: identify contradictions in requested shifts

--- shift_scheduler/configurations/worker.py
+++ shift_scheduler/configurations/worker.py
@@ -1,5 +1,6 @@
 """Module of `WorkerConfiguration`."""
 
+import itertools
 from datetime import date
 
 from pydantic import BaseModel, Field, field_validator
@@ -40,5 +41,25 @@ class WorkerPreferences(BaseModel):
 
         if len(birthday_leaves) > 1:
             raise ValueError("Birthday leave cannot be set to multiple dates.")
+
+        return requests
+
+    @field_validator("requests", mode="after")
+    @classmethod
+    def _ensure_no_contradicted_requests(
+        cls, requests: dict[types.Shift, set[date]]
+    ) -> dict[types.Shift, set[date]]:
+        """Ensures that birthday leave cannot be set to multiple dates."""
+
+        for (shift_a, dates_a), (shift_b, dates_b) in itertools.combinations(requests.items(), 2):
+            if contradictions := dates_a & dates_b:
+                contradictions_str = str.join(
+                    ",", (requested_date.isoformat() for requested_date in contradictions)
+                )
+
+                raise ValueError(
+                    f"Must not request the same date(s) for {shift_a} and {shift_b}: "
+                    + contradictions_str
+                )
 
         return requests
