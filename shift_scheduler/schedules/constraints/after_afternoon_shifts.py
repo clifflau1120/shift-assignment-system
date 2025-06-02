b942c88ffbbde5b6e2cb26e468e9e1904da7cb2c
Cliff Lau <cliff@clifflau.hk>
2025-06-02 13:10:17 +0800

    fix: soft constraint of PA shifts

--- shift_scheduler/schedules/constraints/after_afternoon_shifts.py
+++ shift_scheduler/schedules/constraints/after_afternoon_shifts.py
@@ -37,22 +37,28 @@ class AfterAfternoonShiftsConstraint(base.ShiftAssignmentConstraint):
     def create_soft_constraints(self) -> base.PenaltyExpression:
         penalty: base.PenaltyExpression = 0
 
-        for (worker, preference), (today, tomorrow) in itertools.product(
-            self._config.workers.items(),
+        for worker, (today, tomorrow) in itertools.product(
+            self._config.workers,
             more_itertools.sliding_window(self._config.period, 2),
         ):
-            if preference.accept_pa_shifts:
-                afternoon_shift_today = sum(
-                    self._shift_assignments.get(worker, shift, today)
-                    for shift in types.Shift.all_afternoon_shifts()
-                )
+            afternoon_shift_today = sum(
+                self._shift_assignments.get(worker, shift, today)
+                for shift in types.Shift.all_afternoon_shifts()
+            )
 
-                morning_shift_tomorrow = sum(
-                    self._shift_assignments.get(worker, shift, tomorrow)
-                    for shift in types.Shift.all_morning_shifts()
-                )
+            morning_shift_tomorrow = sum(
+                self._shift_assignments.get(worker, shift, tomorrow)
+                for shift in types.Shift.all_morning_shifts()
+            )
 
-                has_pa_shift = (afternoon_shift_today + morning_shift_tomorrow) == 2
-                penalty += has_pa_shift * self._penalty_per_unit
+            should_penalize = self._model.new_bool_var("")
+            self._model.add((afternoon_shift_today + morning_shift_tomorrow) >= 2).only_enforce_if(
+                should_penalize
+            )
+            self._model.add((afternoon_shift_today + morning_shift_tomorrow) < 2).only_enforce_if(
+                ~should_penalize
+            )
+
+            penalty += should_penalize * self._penalty_per_unit
 
         return penalty
